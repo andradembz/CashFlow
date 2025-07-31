aa2bcebb4f6efbb2b22f3ba052de57829f578359
Miguel Bodo de Andrade <miguel.bodo.andrade@gmail.com>
2025-07-31 10:52:43 -0300

    feat: implementing login

--- src/CashFlow.Application/obj/Debug/net8.0/CashFlow.Application.AssemblyInfo.cs
+++ src/CashFlow.Application/obj/Debug/net8.0/CashFlow.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05431edbac0c59cdb8a5de5466a7d77c354efb42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcea336570310c4f177140d65a275825af9bfdf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/CashFlow.Infraestructure/obj/Debug/net8.0/CashFlow.Infraestructure.AssemblyInfo.cs
+++ src/CashFlow.Infraestructure/obj/Debug/net8.0/CashFlow.Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05431edbac0c59cdb8a5de5466a7d77c354efb42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcea336570310c4f177140d65a275825af9bfdf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
