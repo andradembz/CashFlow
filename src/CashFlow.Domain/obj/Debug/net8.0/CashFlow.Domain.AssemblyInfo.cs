c9379f79a7d3a439b663122603ff452ea209ef87
Miguel Bodo de Andrade <miguel.bodo.andrade@gmail.com>
2025-08-11 08:25:07 -0300

    feat: adding migrations for tags

--- src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
+++ src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b745e490edc1ac0a5e87526c67482955e78f3789")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a708a0d55d39a174059967203e6087c062f9dc7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
